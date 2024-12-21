83fb36627cab2d386a1d3b844e5c3edf3f0a3079
Frederik P. <frepbh@gmail.com>
2024-12-22 00:54:03 +0100

    added a nice error page

--- frontend/src/lib/generic/HorizontalSpacer.svelte
+++ frontend/src/lib/generic/HorizontalSpacer.svelte
@@ -13,10 +13,13 @@
     .spacer {
         display: inline;
         box-sizing: content-box;
+
         height: var(--height, 25%);
         width: var(--thickness, 2px);
+
         background-color: var(--color, var(--tertiary-color));
         background-clip: content-box;
+
         margin-top: var(--margin-top, 0px);
         margin-bottom: var(--margin-bottom, 0px);
         margin-left: var(--margin-left, 0px);
---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import HorizontalSpacer from "$lib/generic/HorizontalSpacer.svelte";
+</script>
+
+<div class="error">
+    <h1 class="code">
+        {page.status}
+    </h1>
+    <HorizontalSpacer
+        --spacing="10px"
+        --thickness="2px"
+        --height="3em"
+        --color="var(--quaternary-color)"
+    />
+    <span class="message">{page.error?.message}</span>
+</div>
+
+<style>
+    .code {
+        font-size: 4em;
+        font-weight: 350;
+        font-family: "Roboto Mono Variable", monospace;
+        padding-bottom: 5px;
+    }
+
+    .message {
+        font-size: 1.8em;
+        font-weight: 200;
+    }
+
+    .error {
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+    }
+</style>
